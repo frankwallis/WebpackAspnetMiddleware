c5d9879f0944f468868aaf34a29319a16de95021
Frank Wallis <fwallis@outlook.com>
2016-03-15 23:44:53 +0000

    chore(travis): trigger build

--- Redouble.AspNet.Webpack/Services/NodeHost.cs
+++ Redouble.AspNet.Webpack/Services/NodeHost.cs
@@ -51,7 +51,7 @@ namespace Redouble.AspNet.Webpack
 
                 await this._client.ConnectAsync("127.0.0.1", _portNumber);
                 this._client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-                this._client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, true);
+                //this._client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, true);
                 this._stream = this._client.GetStream();
 
                 Task.Factory.StartNew(this.ReceiveAll);
