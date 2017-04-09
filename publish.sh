8e11c14321c475778cc25acbdc52a7f11cd55ded
Frank Wallis <fwallis@outlook.com>
2017-04-09 10:54:12 +0100

    add solution file, update build scripts

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm -rf Redouble.AspNet.Webpack/bin
-dotnet restore Redouble.AspNet.Webpack
-dotnet build Redouble.AspNet.Webpack
+dotnet restore
+dotnet build
 dotnet pack Redouble.AspNet.Webpack --configuration Release
+dotnet nuget push ./Redouble.AspNet.Webpack/bin/Release/Redouble.AspNet.Webpack.*.nupkg
