eebdb636a44f8fae1176ff5464c749b41b59de86
Frank Wallis <fwallis@outlook.com>
2017-08-07 09:41:40 +0100

    update nuget publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -v
 rm -rf Redouble.AspNet.Webpack/bin
 dotnet restore
 dotnet build
 dotnet pack Redouble.AspNet.Webpack --configuration Release
-dotnet nuget push ./Redouble.AspNet.Webpack/bin/Release/Redouble.AspNet.Webpack.*.nupkg
+dotnet nuget push ./Redouble.AspNet.Webpack/bin/Release/Redouble.AspNet.Webpack.*.nupkg -s https://nuget.org -k $NUGET_APIKEY
